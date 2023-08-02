895d2fe97e31e7d68c2a89417ef00c773a16134e
Priscilabiazotti <priscilabiazotti@gmail.com>
2023-08-02 18:43:27 -0300

    automação

--- arquivoautomacao.py
+++ arquivoautomacao.py
@@ -20,24 +20,23 @@ pyautogui.write("1234")
 pyautogui.click(x=672, y=585) 
 time.sleep(3)
  
-# Passo 3: Importar a base de produtos pra cadastrar
+
 import pandas as pd
 
 tabela = pd.read_csv("produtos.csv")
 
 print(tabela)
 
-# Passo 4: Cadastrar um produto
 for linha in tabela.index:
-    # clicar no campo de código
+    
     pyautogui.click(x=557, y=185)
-    # pegar da tabela o valor do campo que a gente quer preencher
+    
     codigo = tabela.loc[linha, "codigo"]
-    # preencher o campo
+    
     pyautogui.write(str(codigo))
-    # passar para o proximo campo
+
     pyautogui.press("tab")
-    # preencher o campo
+  
     pyautogui.write(str(tabela.loc[linha, "marca"]))
     pyautogui.press("tab")
     pyautogui.write(str(tabela.loc[linha, "tipo"]))
@@ -52,7 +51,7 @@ for linha in tabela.index:
     if not pd.isna(obs):
         pyautogui.write(str(tabela.loc[linha, "obs"]))
     pyautogui.press("tab")
-    pyautogui.press("enter") # cadastra o produto (botao enviar)
-    # dar scroll de tudo pra cima
+    pyautogui.press("enter")
+    
     pyautogui.scroll(5000)
-    # Passo 5: Repetir o processo de cadastro até o fim
+    
